2fd1364ce799b3f1898e055e2ca0786b29258b20
juvin23 <juvin1234@gmail.com>
2021-06-30 09:10:04 +0700

    30-06-2021 Order Page

--- order.js
+++ order.js
@@ -2,14 +2,15 @@ $('document').ready(main)
 
 const CART_KEY = "CART_STORAGE";
 
-const BURGER = [
-    {id:'Burger1', nama :'Burger Alaska', price : 57000, img : "/imgs/Product/Burger/Burger1.png"},
-    {id:'Burger2', nama :'Burger Malamute', price : 67000, img : "/imgs/Product/Burger/Burger2.png"},
-    {id:'Burger3', nama :'Burger Husky', price : 65000, img : "/imgs/Product/Burger/Burger3.png"}
+const PRODUCT = [
+    {id:'Burger1', cat:'Burger', name :'Burger Alaska', price : 57000, img : "/imgs/Product/Burger/Burger1.png"},
+    {id:'Burger2', cat:'Burger',name :'Burger Malamute', price : 67000, img : "/imgs/Product/Burger/Burger2.png"},
+    {id:'Burger3', cat:'Burger',name :'Burger Husky', price : 65000, img : "/imgs/Product/Burger/Burger3.png"},
+    {id:'side1', cat:'SideDish',name :'Fries', price : 37000, img : "/imgs/Product/SideDish/fries.png"}
 ];
 
 const SIDE = [
-    {id:'side1', nama :'Fries', price : 37000, img : "/imgs/Product/SideDish/fries.png"}
+   
 ];
 
 const CART = [];
@@ -22,28 +23,50 @@ function initItems(){
     const burger_list = $('#burgerList');
     const side_list = $('#sideList');
 
-    BURGER.forEach(key =>{
-        burger_list.append(
-        `<div class="product">
-            <img src= ${key.img}>
-            <H3>${key.nama}</H3>
-            <a href="" class="button"> ADD TO CART </a>
-        </div> `
-        )
-    })
+    PRODUCT.forEach(key =>{
+        if(key.cat === 'Burger'){
+            burger_list.append(
+                `<div class="product">
+                    <img src= ${key.img}>
+                    <H2>${key.name}</H2>
+                    <H3>Rp.${key.price}</H3>
+                    <button onClick=addCart("${key.id}") class="button" > ADD TO CART </button> 
+                </div> `
+                )
+        }else{
+            side_list.append(
+                `<div class="product">
+                    <img src= ${key.img}>
+                    <H3 class = "match-Dark">${key.name}</H3>
+                    <H3>Rp.${key.price}</H3>
+                    <button class="button" onClick=addCart("${key.id}")> ADD TO CART </button>
+                </div> `
+                )
+        }
 
-    SIDE.forEach(key =>{
-        side_list.append(
-        `<div class="product">
-            <img src= ${key.img}>
-            <H3>${key.nama}</H3>
-            <button class="button" onClick=addCart(${key.id})> ADD TO CART </button>
-        </div> `
-        )
     })
 }
 
-function addCart(key){
+function addCart(id){
+    const storage = getLocal(CART_KEY);
+    const newItem = PRODUCT.find(key =>{
+        return key.id === id;
+    })
+
+    if(!storage){
+        newItem.total = 1;
+        setLocal(CART_KEY,[newItem]);
+        return;
+    }else{
+        const exist = storage.find(key =>{
+            if(key.id === id) return key.total++;
+        })
+        if(!exist){
+            newItem.total = 1;
+            storage.push(newItem);
+        }
+        setLocal(CART_KEY,storage);
+    }
 
 }
 
